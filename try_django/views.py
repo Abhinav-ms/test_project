65fe34aec252f9c454b982a3796f7a033d817dcd
Abhinav M S <abhinav@ycatechnologies.com>
2019-08-12 17:28:48 +0530

    new project

---
+++ try_django/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+
+def home_page(request):
+  my_title = "Hello there...."
+  context = {'title': my_title}
+  if request.user.is_authenticated:
+    context = {"title": my_title, "my_list": [1,2,3,4,5,6]}
+  # template_name = "title.txt"
+  # template_obj = get_template(template_name)
+  # rendered_string = template_obj.render(context)
+  # doc = "<h1>{title}</h1>".format(title=title)
+  # django_rendered_doc = "<h1>{{title}</h1>".format(title=title)
+  return render(request, "home.html", context)
+
+def about_page(request):
+  return render(request, "about.html", {"title": "Aboout us"})
+
+def contact_page(request):
+  return render(request, "hello_world.html", {"title": "Contacct us"})
+
+def example_page(request):
+  context = {"title": "Example"}
+  template_name = "hello_world.html"
+  template_obj = get_template(template_name)
+  rendered_item = template_obj.render(context)
+  return HttpResponse(rendered_item)
