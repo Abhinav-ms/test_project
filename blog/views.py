febe5fbded806ef2fc817063c63193f3d926fa10
Abhinav M S <abhinav@ycatechnologies.com>
2019-08-19 11:54:59 +0530

    published date, timestamp and updated

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ from .models import BlogPost
 @staff_member_required
 def blog_post_list_view(request):
   # qs = BlogPost.objects.filter(title__icontains='world')
-  qs = BlogPost.objects.all()
+  qs = BlogPost.objects.all().published()
   template_name = 'blog/list.html'
   context = {'object_list': qs}
   return render(request, template_name, context)
